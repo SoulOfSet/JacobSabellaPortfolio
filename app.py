627d27e59f96bda5d568e9e39551ff3c92b95f88
SoulOfSet <jacobsabella@yahoo.com>
2020-02-03 13:13:58 -0600

    Add comments. Test Jenkie

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.portfolio import portfolio_routes
 app = Flask(__name__)
 app.register_blueprint(portfolio_routes, url_prefix='/portfolio')
 
+# Start App
 if __name__ == "__main__":
     app.run()
 
