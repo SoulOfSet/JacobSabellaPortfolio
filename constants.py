a843e33ae9a475482e79402a7c3b2cfa1f68d057
Jacob Sabella <jacobsabella@yahoo.com>
2020-02-05 18:33:32 -0600

    Added some constants for projects

--- constants.py
+++ constants.py
@@ -1,5 +1,17 @@
+from models.project import Project
+
 PORTFOLIO_NAME = "Jacob Sabella"
 PORTFOLIO_TITLE = "Software Developer"
 PORTFOLIO_DESCRIPTION = "Software developer specializing in back-end web and batch processes. Expert in " \
                         "Java/SQL/PLSQL. Experience with web technologies such as Flask, Node.js, and Spring Boot. " \
-                        "Reluctantly admitting COBOL experience. Projects completed in Unity3D and GIS related apps."
+                        "Reluctantly admitting COBOL exposure. Projects worked on in Unity3D and GIS related apps."
+projects = []
+
+projects[0] = Project("Twitics", "Given a Twitch user, show users that are not following them back.", None,
+                      "https://twitics.jacobsabella.com")
+
+projects[1] = Project("Associate Toolkit", "Android app for WalMart associates that displays barcodes for given items.",
+                      None, "https://play.google.com/store/apps/details?id=com.soulworks.soulofset.associatetoolkit")
+
+projects[2] = Project("Jacob Sabella Portfolio", "This website. Built on Flask using Bootstrap for the front end",
+                      None, "https://jacobsabella.com")
---
+++ models/project.py
@@ -0,0 +1,6 @@
+class Project:
+    def __init__(self, name, description, github_link, demo_link):
+        self.demo_link = demo_link
+        self.github_link = github_link
+        self.name = name
+        self.description = description
