e3c29d65db29f9640e1751f0f3f3205af3a06dfd
SoulOfSet <jacobsabella@yahoo.com>
2020-02-10 08:25:04 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,11 +7,11 @@ PORTFOLIO_DESCRIPTION = "Software developer specializing in back-end web and bat
                         "Reluctantly admitting COBOL exposure. Projects worked on in Unity3D and GIS related apps."
 projects = []
 
-projects[0] = Project("Twitics", "Given a Twitch user, show users that are not following them back.", None,
-                      "https://twitics.jacobsabella.com")
+projects.append(Project("Twitics", "Given a Twitch user, show users that are not following them back.", None,
+                      "https://twitics.jacobsabella.com"))
 
-projects[1] = Project("Associate Toolkit", "Android app for WalMart associates that displays barcodes for given items.",
-                      None, "https://play.google.com/store/apps/details?id=com.soulworks.soulofset.associatetoolkit")
+projects.append(Project("Associate Toolkit", "Android app for WalMart associates that displays barcodes for given items.",
+                      None, "https://play.google.com/store/apps/details?id=com.soulworks.soulofset.associatetoolkit"))
 
-projects[2] = Project("Jacob Sabella Portfolio", "This website. Built on Flask using Bootstrap for the front end",
-                      None, "https://jacobsabella.com")
+projects.append(Project("Jacob Sabella Portfolio", "This website. Built on Flask using Bootstrap for the front end",
+                      None, "https://jacobsabella.com"))
